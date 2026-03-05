ba8d7a298bc01fd66b9f8eef393bf9e4c8285439
Olivier Desenfans <desenfans.olivier@gmail.com>
2026-03-05 10:32:35 +0100

    load init from rootfs

--- nix/rootfs.nix
+++ nix/rootfs.nix
@@ -1,11 +1,23 @@
 { pkgs, fib-service, ... }:
 
+let
+  staticBusybox = pkgs.busybox.override { enableStatic = true; };
+in
 pkgs.runCommand "rootfs.ext4" {
   nativeBuildInputs = [ pkgs.e2fsprogs ];
 } ''
-  # Create a minimal ext4 image.
-  mkdir -p rootfs/bin
+  # Create a minimal ext4 image with /sbin/init entrypoint.
+  mkdir -p rootfs/sbin rootfs/bin
   cp ${fib-service}/bin/fib-service rootfs/bin/
+  cp ${staticBusybox}/bin/busybox rootfs/bin/
+
+  # /sbin/init is the rootfs entrypoint convention.
+  # Heredoc must not be indented (shebang needs to start at column 0).
+  cat > rootfs/sbin/init <<'INIT'
+#!/bin/busybox sh
+exec /bin/fib-service
+INIT
+  chmod +x rootfs/sbin/init
 
   # Calculate size (add 10MB padding).
   size=$(du -sm rootfs | cut -f1)
